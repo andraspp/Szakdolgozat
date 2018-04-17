802090a7f013077b920cc209d803f753d2511427
Andras Papp <andraspp@gmail.com>
2018-04-17 21:22:50 +0200

    Turning avoidance more or less works...

--- autonomous_vehicle/src/av_control/av_control_common.h
+++ autonomous_vehicle/src/av_control/av_control_common.h
@@ -6,6 +6,7 @@
 #include <ros/ros.h>
 #include <std_msgs/Bool.h>
 #include <std_msgs/String.h>
+#include <geometry_msgs/Twist.h>
 #include <sensor_msgs/LaserScan.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <sensor_msgs/Image.h>
@@ -28,34 +29,75 @@
 #define     AvTrue              (1)
 #define     AvFalse             (0)
 #define     AvFrontAngle        (320)
-#define     AvFrontAngleOffset  (20)
-#define     AvMaxObjects        (100)
+#define     AvFrontAngleOffset  (5)
+#define     AvMaxObjects        (10)
+#define     AvTmpAngleCorr      (0)
+
+/* Object range thresholds */
+#define     AvProxyFarThr       (7)
+#define     AvProxyCloseThr     (4)
+#define     AvProxyDngrThr      (2)
+#define     AvProxyHyst         (0.1)
+
+/* DEBUG INFO handling */
+#define AvDebugSpeedInfoEnable          (0x01)
+#define AvDebugObjectsStateInfoEnable   (0x02)
+#define AvDebugStoredObjectsInfoEnable  (0x04)
+#define AvDebugFrontDetInfoEnable       (0x08)
+
+#define AvDebugConfig                   (AvDebugSpeedInfoEnable | AvDebugObjectsStateInfoEnable | AvDebugStoredObjectsInfoEnable | AvDebugFrontDetInfoEnable)
 
 sensor_msgs::PointCloud2             cloud_msg;
 const sensor_msgs::PointCloud2ConstPtr& cloud_msg_ptr = boost::make_shared<sensor_msgs::PointCloud2>(cloud_msg);
 pcl::VoxelGrid<pcl::PCLPointCloud2> sor;
 
+
+typedef enum Av_proximity_level_e {
+    AvProximityFar          = 0,
+    AvProximityClose        = 2,
+    AvProximityDanger       = 3,
+    AvProximityUnknown      = 10
+} Av_proximity_level_t;
+
 typedef struct Av_object_s {
-    signed int Av_obj_id;
-    signed int Av_scan_low_point;
-    signed int Av_scan_high_point;
-    double     Av_object_range_avg;
+    signed int              Av_obj_id;
+    signed int              Av_scan_low_point;
+    signed int              Av_scan_high_point;
+    double                  Av_object_range_avg;
+    unsigned int            Av_object_in_front;
+    Av_proximity_level_t    Av_object_proximity;
 } Av_objects_t;
 
-Av_objects_t    Av_object_container[AvMaxObjects];
-bool            Av_scan_detection;
-bool            Av_scan_detection_LL;
-unsigned int    Av_num_of_objects;
+typedef struct Av_orientation_s {
+    float z; // sin (theta / 2)
+    float w; // cos (theta / 2)
 
-int DEBUG_INFO;
+    Av_orientation_s(void) : w(0.0f), z(0.0f) {}
+    Av_orientation_s(float _w, float _z) : w(_w), z(_z) {}
+} Av_orientation_t;
+
+Av_objects_t        Av_object_container[AvMaxObjects];
+
+bool                Av_scan_detection;
+bool                Av_scan_detection_LL;
+unsigned int        Av_num_of_objects;
+Av_orientation_t    Av_robot_orientation;
+
+
+ros::Publisher  Av_cmd_vel_pub;
+
 
 void AV_INIT(void);
 void AV_SENSORICS_LIDAR_CALLBACK(const sensor_msgs::LaserScan::ConstPtr& scan);
-void AV_CLOUD_CB (void);
+void AV_ODOMETRY_CALLBACK(const nav_msgs::Odometry::ConstPtr& odom);
+
+void AV_SET_VELO(float lin_vel, float ang_vel);
+
+void AV_SET_OBJECT_PROXIMITY(void);
 void AV_DETECT_FRONT_OBJECT (void);
 void AV_CLEAR_OBJECTS_ARRAY(void);
-
-unsigned int AV_NUM_OF_OBJECTS(void);
+int AV_NUM_OF_OBJECTS(void);
 bool AV_OBJECT_HAS_ELEMENTS(void);
+void AV_COLLISION_AVOIDANCE(void);
 
 #endif
--- autonomous_vehicle/src/av_control/av_sensorics.cpp
+++ autonomous_vehicle/src/av_control/av_sensorics.cpp
@@ -5,9 +5,8 @@ int main(int argc, char **argv)
 {
 
     ros::init(argc, argv, "av_sensorics");
-    AV_INIT();
+    
 
-    DEBUG_INFO = 0;
     // Exit if no ROS
     if (!ros::isInitialized())
     {
@@ -21,12 +20,17 @@ int main(int argc, char **argv)
         std::cout << "ROS has been properly initialized :-) " << std::endl;
         ros::NodeHandle av_sensorics_nh_;
 
+        //ros::Subscriber AV_ODOM_CHECK  = av_sensorics_nh_.subscribe<sensor_msgs::LaserScan>("/av_robot/odom",sizeof(nav_msgs::Odometry),&AV_ODOMETRY_CALLBACK);
+        ros::topic::waitForMessage<nav_msgs::Odometry>("/av_robot/odom");
         ros::Subscriber AV_LIDAR_CHECK = av_sensorics_nh_.subscribe<sensor_msgs::LaserScan>("/av_robot/laser/scan",10,&AV_SENSORICS_LIDAR_CALLBACK);
         
+        Av_cmd_vel_pub = av_sensorics_nh_.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
+        AV_INIT();
+
+        AV_SET_VELO(0.0, 2.0);
         ros::Rate loop_rate(10);
         while(ros::ok())
         {
-            AV_DETECT_FRONT_OBJECT();
             ros::spinOnce();
             loop_rate.sleep();
         }
@@ -41,34 +45,42 @@ void AV_INIT()
 {
     AV_CLEAR_OBJECTS_ARRAY();
     AV_OBJECT_HAS_ELEMENTS();
+    AV_SET_VELO(0.0, 0.0);
 }
 
 void AV_CLEAR_OBJECTS_ARRAY()
 {
-    for(int oidx=0; oidx < AvMaxObjects; oidx++)
+    for(int idx=0; idx < AvMaxObjects; idx++)
     {
-        Av_object_container[oidx].Av_obj_id      = -1;
-        Av_object_container[oidx].Av_scan_low_point = -1;
-        Av_object_container[oidx].Av_scan_high_point = -1;
-        Av_object_container[oidx].Av_object_range_avg = -1.00;
+        Av_object_container[idx].Av_obj_id             = -1;
+        Av_object_container[idx].Av_scan_low_point     = -1;
+        Av_object_container[idx].Av_scan_high_point    = -1;
+        Av_object_container[idx].Av_object_range_avg   = -1.00;
+        Av_object_container[idx].Av_object_in_front    = AvFalse;
+        Av_object_container[idx].Av_object_proximity   = AvProximityFar;
     }
 }
 
 bool AV_OBJECT_HAS_ELEMENTS()
 {
+    bool ret_val;
+
     if (Av_object_container[0].Av_obj_id != -1)
     {
-        return AvTrue;
+        ret_val = AvTrue;
     }
     else
     {
-        return AvFalse;
+        ret_val = AvFalse;
     }
+
+    ROS_INFO("AV_OBJECT_HAS_ELEMENTS return value: %d", ret_val);
+    return ret_val;
 }
 
-unsigned int AV_NUM_OF_OBJECTS()
+int AV_NUM_OF_OBJECTS()
 {
-    unsigned int oidx;
+    int oidx;
 
     oidx = 0;
 
@@ -80,14 +92,8 @@ unsigned int AV_NUM_OF_OBJECTS()
     return oidx;
 }
 
-void AV_DETECT_FRONT_OBJECT()
-{
-    
-}
-
 void AV_SENSORICS_LIDAR_CALLBACK(const sensor_msgs::LaserScan::ConstPtr& scan)
 {
-    tf::TransformBroadcaster broadcaster;
     laser_geometry::LaserProjection projector;
     float sum;
     unsigned int cnt;
@@ -107,6 +113,7 @@ void AV_SENSORICS_LIDAR_CALLBACK(const sensor_msgs::LaserScan::ConstPtr& scan)
         {
             if(   (scan->ranges[scan_idx] <= scan->range_max)
                && (scan->ranges[scan_idx] >= scan->range_min)
+               && (num_of_objects         <  AvMaxObjects)
             )
             {
                 object_detected = AvTrue;
@@ -146,19 +153,35 @@ void AV_SENSORICS_LIDAR_CALLBACK(const sensor_msgs::LaserScan::ConstPtr& scan)
         }
 
         Av_num_of_objects = (unsigned char)AV_NUM_OF_OBJECTS();
-        std::cout << std::endl;
-        std::cout << "temp num of objects: " << num_of_objects << " stored num of objects: " << Av_num_of_objects << std::endl;
+        #if((AvDebugConfig & AvDebugStoredObjectsInfoEnable) > 0)
+        ROS_INFO("temp num of objects: %d, stored num of objects: %d", num_of_objects, Av_num_of_objects);
+        ROS_INFO("Object 0's ID: %d", Av_object_container[0].Av_obj_id);
+        #endif /* AvDebugStoredObjectsInfoEnable */
         
 
-        if(AV_OBJECT_HAS_ELEMENTS() == AvTrue)
+        if(Av_num_of_objects > 0) /* There are tracked objects */
         {
+            AV_SET_OBJECT_PROXIMITY();  /* Set their proximity state */
+            AV_DETECT_FRONT_OBJECT();   /* Detect those that are in front of the AV */
+
+            #if((AvDebugConfig & AvDebugObjectsStateInfoEnable) > 0)
             for(int idx; idx < Av_num_of_objects; idx++)
             {
-                std::cout << std::endl;
-                std::cout << "Object No: " << Av_object_container[idx].Av_obj_id << std::endl;
-                std::cout << "Object Start Angle: " << Av_object_container[idx].Av_scan_low_point << " Object End Angle: " << Av_object_container[idx].Av_scan_high_point << std::endl;
-                std::cout << "Object average range: " << Av_object_container[idx].Av_object_range_avg << std::endl;
+                ROS_INFO(" ");
+                ROS_INFO("Object No: %d", Av_object_container[idx].Av_obj_id);
+                ROS_INFO("Object Start Angle: %d  Object End Angle: %d", Av_object_container[idx].Av_scan_low_point, Av_object_container[idx].Av_scan_high_point);
+                ROS_INFO("Object average range: %1.8f", Av_object_container[idx].Av_object_range_avg);
+                ROS_INFO("Object proximity state: %d", Av_object_container[idx].Av_object_proximity);
+                ROS_INFO("Object front?: %d", Av_object_container[idx].Av_object_in_front);
             }
+            #endif /* AvDebugObjectsStateInfoEnable */
+            AV_COLLISION_AVOIDANCE();
+            
+        }
+        else
+        {
+            /* proceed with normal speed */
+            AV_SET_VELO(0.0, 1.0);
         }
         
     }
@@ -168,3 +191,178 @@ void AV_SENSORICS_LIDAR_CALLBACK(const sensor_msgs::LaserScan::ConstPtr& scan)
         return;
     }
 }
+
+void AV_SET_OBJECT_PROXIMITY()
+{
+    for(int idx; idx < Av_num_of_objects; idx++)
+    {
+        if(Av_object_container[idx].Av_object_range_avg >= AvProxyFarThr) /* If range is really far, don't bother with state check */
+        {
+            Av_object_container[idx].Av_object_proximity = AvProximityFar;
+        }
+        else
+        {
+            switch(Av_object_container[idx].Av_object_proximity)
+            {
+            case AvProximityFar:
+                if (Av_object_container[idx].Av_object_range_avg >= AvProxyCloseThr)
+                {
+                    continue; /* Stay in Far state */
+                }
+                else if( Av_object_container[idx].Av_object_range_avg < AvProxyDngrThr)
+                {
+                    Av_object_container[idx].Av_object_proximity = AvProximityDanger;
+                }
+                else if( Av_object_container[idx].Av_object_range_avg < AvProxyCloseThr)
+                {
+                    Av_object_container[idx].Av_object_proximity = AvProximityClose;
+                }
+                else
+                {
+                    /* Stay in Far state */
+                }
+                break;
+            case AvProximityClose:
+                if (   (Av_object_container[idx].Av_object_range_avg >= AvProxyDngrThr) 
+                    && (Av_object_container[idx].Av_object_range_avg <= AvProxyCloseThr)
+                )
+                {
+                    continue; /* Stay in Close state */
+                }
+                else if( Av_object_container[idx].Av_object_range_avg < (AvProxyDngrThr - AvProxyHyst))
+                {
+                    Av_object_container[idx].Av_object_proximity = AvProximityDanger;
+                }
+                else if(Av_object_container[idx].Av_object_range_avg > (AvProxyCloseThr + AvProxyHyst))
+                {
+                    Av_object_container[idx].Av_object_proximity = AvProximityFar;
+                }
+                else
+                {
+                    /* Stay in Close state */
+                }
+                break;
+            case AvProximityDanger:
+                if (Av_object_container[idx].Av_object_range_avg <= AvProxyDngrThr) 
+                {
+                    continue; /* Stay in Close state */
+                }
+                else if(Av_object_container[idx].Av_object_range_avg > (AvProxyCloseThr + AvProxyHyst))
+                {
+                    Av_object_container[idx].Av_object_proximity = AvProximityFar;
+                }
+                else if(Av_object_container[idx].Av_object_range_avg > (AvProxyDngrThr + AvProxyHyst))
+                {
+                    Av_object_container[idx].Av_object_proximity = AvProximityClose;
+                }
+                else
+                {
+                    /* Stay in Close state */
+                }
+                break;
+            case AvProximityUnknown:
+            default:
+                if( Av_object_container[idx].Av_object_range_avg > AvProxyCloseThr )
+                {
+                    Av_object_container[idx].Av_object_proximity = AvProximityFar;
+                }
+                else if( Av_object_container[idx].Av_object_range_avg > AvProxyDngrThr )
+                {
+                    Av_object_container[idx].Av_object_proximity = AvProximityClose;
+                }
+                else
+                {
+                    Av_object_container[idx].Av_object_proximity = AvProximityDanger;
+                }
+                break;
+            }
+        }
+    }
+}
+
+void AV_DETECT_FRONT_OBJECT()
+{
+    #if((AvDebugConfig & AvDebugFrontDetInfoEnable) > 0)
+    ROS_INFO("Front Det entered");
+    #endif /* AvDebugFrontDetInfoEnable */
+
+    for(int idx; idx < Av_num_of_objects; idx++)
+    {
+        if(   ((AvFrontAngle + AvFrontAngleOffset + AvTmpAngleCorr) >= Av_object_container[idx].Av_scan_low_point  <= (AvFrontAngle - AvFrontAngleOffset + AvTmpAngleCorr)) /*TODO: implement turn angle correction*/
+           || ((AvFrontAngle + AvFrontAngleOffset + AvTmpAngleCorr) >= Av_object_container[idx].Av_scan_high_point >= (AvFrontAngle - AvFrontAngleOffset + AvTmpAngleCorr))
+           || (   ((AvFrontAngle + AvFrontAngleOffset + AvTmpAngleCorr) <= Av_object_container[idx].Av_scan_high_point) 
+               && ((AvFrontAngle - AvFrontAngleOffset + AvTmpAngleCorr) >= Av_object_container[idx].Av_scan_low_point)
+              )
+          )
+        {
+            #if((AvDebugConfig & AvDebugFrontDetInfoEnable) > 0)
+            ROS_INFO("Object ID: %d detected to the front!!", Av_object_container[idx].Av_obj_id);
+            #endif /* AvDebugFrontDetInfoEnable */
+            Av_object_container[idx].Av_object_in_front = AvTrue;
+        }
+        else
+        {
+            #if((AvDebugConfig & AvDebugFrontDetInfoEnable) > 0)
+            ROS_INFO("Object ID: %d is NOT in front!!", Av_object_container[idx].Av_obj_id);
+            #endif /* AvDebugFrontDetInfoEnable */
+            Av_object_container[idx].Av_object_in_front = AvFalse;
+        }
+
+    }
+}
+
+void AV_SET_VELO(float lin_vel, float ang_vel)
+{
+    geometry_msgs::Twist msg;
+    msg.linear.x = lin_vel;
+    msg.angular.z = ang_vel;
+
+    #if((AvDebugConfig & AvDebugSpeedInfoEnable) > 0)
+    ROS_INFO(" ");
+    ROS_INFO("Robot ang speed: %1.8f Robot lin speed: %1.8f", msg.angular.z, msg.linear.x);
+    #endif /* AvDebugSpeedInfoEnable */
+
+    Av_cmd_vel_pub.publish(msg);
+}
+
+void AV_COLLISION_AVOIDANCE()
+{
+    for(int idx; idx < Av_num_of_objects; idx++)
+    {
+        if(   (Av_object_container[idx].Av_object_in_front  == AvTrue)
+           && (Av_object_container[idx].Av_object_proximity == AvProximityDanger)
+        )
+        {
+            /* stop to avoid collision */
+            AV_SET_VELO(0.0, 0.0);
+        }
+        else if(   (Av_object_container[idx].Av_object_in_front == AvTrue)
+               && (Av_object_container[idx].Av_object_proximity == AvProximityClose)
+        )
+        {
+            /* slow down and turn to avoid collision */
+            AV_SET_VELO(0.25, 0.5);
+        }
+        /*else if(   (Av_object_container[idx].Av_object_in_front  == AvFalse)
+                && (Av_object_container[idx].Av_object_proximity == AvProximityDanger)
+        )
+        {
+            if(Av_object_container[idx].Av_scan_low_point > 320)
+            {
+
+            }
+        }*/
+        else
+        {
+            /* no change in course necessary */
+            AV_SET_VELO(0.0, 1.0);
+        }
+    }
+}
+
+/*
+void AV_ODOMETRY_CALLBACK(const nav_msgs::Odometry::ConstPtr& odom)
+{
+    Av_robot_orientation.z = odom->pose.pose.orientation.z;
+    Av_robot_orientation.w = odom->pose.pose.orientation.w;
+}*/
